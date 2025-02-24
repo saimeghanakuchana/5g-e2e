fd27d22cad9b75d5717a27c98f23cc705419e0cc
Dustin Maas <dmaas@flux.utah.edu>
2025-02-24 13:28:44 -0700

    Remove tune-sdr-iface service (no exp interface ready).

--- profile.py
+++ profile.py
@@ -43,7 +43,6 @@ core_gnb_node = request.RawPC("core-gnb")
 core_gnb_node.component_manager_id = COMP_MANAGER_ID
 core_gnb_node.disk_image = params.gnb_node_image
 core_gnb_node.component_id = "skull2"
-core_gnb_node.addService(pg.Execute(shell="bash", command="/local/repository/bin/tune-sdr-iface.sh"))
 if params.deploy_o5gsrsran_patched:
     core_gnb_node.addService(pg.Execute(shell="bash", command="/local/repository/bin/deploy-o5gsrsran-patched.sh"))
 core_gnb_node.startVNC()
